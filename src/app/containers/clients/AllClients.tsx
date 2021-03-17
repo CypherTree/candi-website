84144d8315d69d11f914bcf3b571eb4d6c28d70a
Mohit Kumar <mk4227525@gmail.com>
2021-03-17 18:36:12 +0530

    text caps for clients

--- src/app/containers/clients/AllClients.tsx
+++ src/app/containers/clients/AllClients.tsx
@@ -145,7 +145,7 @@ const AllClients = () => {
                   padding: "0",
                 }}
               >
-                Clients
+                CLIENTS
               </p>
 
               <div
